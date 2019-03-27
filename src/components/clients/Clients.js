21476bd2ae66a6e4f12bb90dc083668af403ec6b
Thomas Johnson <tjohnson1106@gmail.com>
2019-03-27 16:09:13 -0400

    created client data map

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Clients extends Component {
   state = {};
@@ -11,6 +12,14 @@ class Clients extends Component {
         email: "janet@mailexample.com",
         phone: "555-555-5555",
         balance: "30"
+      },
+      {
+        id: "434367",
+        firstName: "Tiara",
+        lastName: "Gibson",
+        email: "tiara@mailexample.com",
+        phone: "555-555-4444",
+        balance: "700.77"
       }
     ];
     // check for clients firebase
@@ -21,30 +30,54 @@ class Clients extends Component {
           <div className="row">
             <div className="col-md-6">
               <h2>
+                {""}
                 <i className="fas fa-users" />
                 Clients
               </h2>
             </div>
+            <div className="col-md-6" />
           </div>
 
-          <table className="table-striped">
-            <div className="thead-inverse">
+          <table className="table table-striped">
+            <thead className="thead-inverse">
               <tr>
                 <th>Name</th>
                 <th>Email</th>
                 <th>Balance</th>
                 <th />
               </tr>
-            </div>
+            </thead>
+            {/* loop through client -> insert table row */}
+            <tbody>
+              {clients.map((client) => (
+                <tr key={client.id}>
+                  {/* column for each piece of data */}
+                  <td>
+                    {client.firstName} {client.lastName}
+                  </td>
+                  <td>{client.email}</td>
+                  <td>${parseFloat(client.balance).toFixed(2)}</td>
+
+                  <td>
+                    <Link
+                      to={`/client/${client.id}`}
+                      className="btn btn-secondary btn-sm"
+                    >
+                      <i className="fas fa-arrow-circle-right" /> Details
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
       );
     } else {
+      return (
+        // TODO implement spinner
+        <h1>Loading...</h1>
+      );
     }
-    return (
-      // TODO implement spinner
-      <h1>Loading...</h1>
-    );
   }
 }
 
