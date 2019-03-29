b506b0796def78738b4d7f4f11d1bf0d387227a6
Thomas Johnson <tjohnson1106@gmail.com>
2019-03-29 18:10:28 -0400

    created compose method for client details

---
+++ src/components/clients/ClientDetails.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+
+import Spinner from "../layouts/Spinner";
+
+class ClientDetails extends Component {
+  state = {};
+  render() {
+    return <div className="">{}</div>;
+  }
+}
+
+export default compose(
+  firestoreConnect((props) => [
+    {
+      collection: "clients",
+      storeAs: "client",
+      doc: props.match.params.id
+    }
+  ]),
+  connect(({ firestore: { ordered } }, props) => ({
+    client: ordered.client && ordered.client[0]
+  }))
+)(ClientDetails);
