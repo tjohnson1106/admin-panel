e7b4ef20cdbda96dde93c28cbd3179c6edcef46f
Thomas Johnson <tjohnson1106@gmail.com>
2019-03-29 14:33:12 -0400

    created initial form  state

--- src/components/clients/AddClient.js
+++ src/components/clients/AddClient.js
@@ -1,11 +1,43 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class AddClient extends Component {
-  state = {};
+  state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    balance: ""
+  };
   render() {
     return (
-      <div className="">
-        <h1>Add Client</h1>
+      <div className="row">
+        <div className="col-md-6">
+          <Link to="/" className="btn-link">
+            <i className="fas fa-arrow-circle-left"> </i>
+            Dashboard
+          </Link>
+        </div>
+
+        <div className="card">
+          <div className="card-header">Add Client</div>
+
+          <div className="card-body">
+            <form>
+              <div className="form-group">
+                <label htmlFor="firstName">First Name</label>
+                {/* input -> state */}
+                <input
+                  type="text"
+                  className="form-control"
+                  name="firstName"
+                  minLength="2"
+                  required
+                />
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
