70a58bff695d87559fbba7f950a4fd6af5decb24
Thomas Johnson <tjohnson1106@gmail.com>
2019-03-29 14:19:30 -0400

    created add client route

---
+++ src/components/clients/AddClient.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class AddClient extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="">
+        <h1>Add Client</h1>
+      </div>
+    );
+  }
+}
+
+export default AddClient;
