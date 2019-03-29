7fea138c1ed040e20886a7439b37d24685776eea
Thomas Johnson <tjohnson1106@gmail.com>
2019-03-29 15:05:05 -0400

    created add client form on submit method

--- src/components/clients/AddClient.js
+++ src/components/clients/AddClient.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+// import { compose } from "redux";
+// import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
 
 class AddClient extends Component {
   state = {
@@ -10,6 +14,24 @@ class AddClient extends Component {
     balance: ""
   };
 
+  _onSubmit = (e) => {
+    e.preventDefault();
+
+    const newClient = this.state;
+
+    const { firestore, history } = this.props;
+
+    // if no balance -> 0
+
+    if (newClient.balance === "") {
+      newClient.balance = 0;
+    }
+
+    firestore
+      .add({ collection: "clients" }, newClient)
+      .then(() => history.push("/"));
+  };
+
   _onChange = (e) =>
     this.setState({
       [e.target.name]: e.target.value
@@ -17,81 +39,88 @@ class AddClient extends Component {
 
   render() {
     return (
-      <div className="row">
-        <div className="col-md-6">
-          <Link to="/" className="btn-link">
-            <i className="fas fa-arrow-circle-left"> </i>
-            Dashboard
-          </Link>
-        </div>
+      <div>
+        <div className="row">
+          <div className="col-md-6">
+            <Link to="/" className="btn-link">
+              <i className="fas fa-arrow-circle-left" />
+              Dashboard
+            </Link>
+          </div>
 
-        <div className="card">
-          <div className="card-header">Add Client</div>
+          <div className="card">
+            <div className="card-header">Add Client</div>
 
-          <div className="card-body">
-            <form>
-              <div className="form-group">
-                <label htmlFor="firstName">First Name</label>
-                {/* input -> state */}
+            <div className="card-body">
+              <form onSubmit={this._onSubmit}>
+                <div className="form-group">
+                  <label htmlFor="firstName">First Name</label>
+                  {/* input -> state */}
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="firstName"
+                    minLength="2"
+                    required
+                    onChange={this._onChange}
+                    value={this.state.firstName}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="lastName">Last Name</label>
+                  {/* input -> state */}
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="lastName"
+                    minLength="2"
+                    required
+                    onChange={this._onChange}
+                    value={this.state.lastName}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  {/* input -> state */}
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    onChange={this._onChange}
+                    value={this.state.email}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="phone">Phone</label>
+                  {/* input -> state */}
+                  <input
+                    type="phone"
+                    className="form-control"
+                    name="phone"
+                    minLength="10"
+                    required
+                    onChange={this._onChange}
+                    value={this.state.phone}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="balance">Balance</label>
+                  {/* input -> state */}
+                  <input
+                    type="balance"
+                    className="form-control"
+                    name="balance"
+                    onChange={this._onChange}
+                    value={this.state.balance}
+                  />
+                </div>
                 <input
-                  type="text"
-                  className="form-control"
-                  name="firstName"
-                  minLength="2"
-                  required
-                  onChange={this._onChange}
-                  value={this.state.firstName}
+                  type="submit"
+                  value="Submit"
+                  className="btn btn-primary btn-block"
                 />
-              </div>
-              <div className="form-group">
-                <label htmlFor="lastName">Last Name</label>
-                {/* input -> state */}
-                <input
-                  type="text"
-                  className="form-control"
-                  name="lastName"
-                  minLength="2"
-                  required
-                  onChange={this._onChange}
-                  value={this.state.lastName}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                {/* input -> state */}
-                <input
-                  type="email"
-                  className="form-control"
-                  name="email"
-                  onChange={this._onChange}
-                  value={this.state.email}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="phone">Phone</label>
-                {/* input -> state */}
-                <input
-                  type="phone"
-                  className="form-control"
-                  name="phone"
-                  minLength="10"
-                  required
-                  onChange={this._onChange}
-                  value={this.state.phone}
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="balance">Balance</label>
-                {/* input -> state */}
-                <input
-                  type="balance"
-                  className="form-control"
-                  name="balance"
-                  onChange={this._onChange}
-                  value={this.state.balance}
-                />
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
         </div>
       </div>
@@ -99,4 +128,8 @@ class AddClient extends Component {
   }
 }
 
-export default AddClient;
+AddClient.propTypes = {
+  firestore: PropTypes.object.isRequired
+};
+
+export default firestoreConnect()(AddClient);
